79461bed8ccc514119b5762cec382a6f2ad1ce0f
lunarydess <blank1337@keemail.me>
2023-12-25 21:19:07 +0100

    :art: updated Arithmetics.cs

--- Math/Arithmetics.cs
+++ Math/Arithmetics.cs
@@ -1,6 +1,20 @@
 ﻿namespace Library_Lilith.Math;
 
 public sealed class Arithmetics {
+    #region Average
+    public static int AverageInt(params int[]    numbers) => numbers.Sum()       / numbers.Length;
+    public static int AverageInt(params float[]  numbers) => (int) numbers.Sum() / numbers.Length;
+    public static int AverageInt(params double[] numbers) => (int) numbers.Sum() / numbers.Length;
+
+    public static float AverageFloat(params int[]    numbers) => numbers.Sum()         / (float) numbers.Length;
+    public static float AverageFloat(params float[]  numbers) => numbers.Sum()         / numbers.Length;
+    public static float AverageFloat(params double[] numbers) => (float) numbers.Sum() / numbers.Length;
+
+    public static double AverageDouble(params int[]    numbers) => numbers.Sum()          / (double) numbers.Length;
+    public static double AverageDouble(params float[]  numbers) => (double) numbers.Sum() / numbers.Length;
+    public static double AverageDouble(params double[] numbers) => numbers.Sum()          / numbers.Length;
+    #endregion
+
     #region Clamp
     public static byte Clamp(in byte value, in byte minimum, in byte maximum)
         => value < minimum ? minimum :
